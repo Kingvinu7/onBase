ae0aea737054a9c1a38285af9f3105c2f0092e7e
VINU07 <88230429+Kingvinu7@users.noreply.github.com>
2025-09-29 18:14:44 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export async function generateMetadata(): Promise<Metadata> {
       card: 'summary_large_image',
       title: minikitConfig.miniapp.ogTitle,
       description: minikitConfig.miniapp.ogDescription,
-      images: [minikitConfig.miniapp.ogImageUrl],
+      images: [minikitConfig.miniapp.ImageUrl],
     },
     other: {
       // Farcaster Frame metadata
@@ -44,7 +44,7 @@ export async function generateMetadata(): Promise<Metadata> {
         splashBackgroundColor: minikitConfig.miniapp.splashBackgroundColor,
         homeUrl: minikitConfig.miniapp.homeUrl,
         imageUrl: minikitConfig.miniapp.heroImageUrl,
-        ogImageUrl: "/onBase-hero.png",
+
         primaryCategory: minikitConfig.miniapp.primaryCategory,
         tags: minikitConfig.miniapp.tags,
         button: {
